c222cb9ff729b469859c912577953fb980ecb481
David-Moisan <dmoisan.23@gmail.com>
2021-11-14 14:41:01 +0100

    Init: React

--- anquan/frontend/views.py
+++ anquan/frontend/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request, *args, **kwargs):
-    return render(request, "templates/index.html")
+    return render(request, "frontend/index.html")
